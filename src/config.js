826f94688cd4f61cf4aab8b8afa1782298f52927
mferret <m.13ferret@gmail.com>
2017-05-22 15:44:10 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 process.env.PORT = '5000'
-process.env.REQUEST_TOKEN = ''
-process.env.LANGUAGE = ''
+process.env.REQUEST_TOKEN = 'b79066637c6fd0649a3bb98b6c11adc8'
+process.env.LANGUAGE = 'fr'
 // Write your own configuration here:
 // ...
